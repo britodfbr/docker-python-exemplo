936f9ed005f58638fee2d2a7b8a4ab7b1c66a4f0
Britodfbr <britodfbr@gmail.com>
2022-04-22 17:16:13 -0300

    refactor: Restruturação do pacote

--- docker_python_exemplo/app.py
+++ docker_python_exemplo/app.py
@@ -9,10 +9,12 @@ port_redis = os.environ.get('PORT_REDIS', 6379)
 app = Flask(__name__)
 redis = Redis(host=host_redis, port=port_redis)
 
+
 @app.route('/')
 def hello():
     redis.incr('hits')
     return 'Hello World! %s times' % redis.get('hits')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
