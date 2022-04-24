539b8ba9b0d491ba6f2b8528f1dec8f8074c1955
Britodfbr <britodfbr@gmail.com>
2022-04-24 09:40:30 -0300

    style: Formatado apresentação do version.txt

--- docker_python_exemplo/__init__.py
+++ docker_python_exemplo/__init__.py
@@ -13,7 +13,7 @@ except ImportError:
 configfile = Path(__file__).parents[1].joinpath("pyproject.toml")
 versionfile = Path(__file__).parent.joinpath("version.txt")
 a = toml.load(configfile.as_posix())['tool']['poetry']['version']
-versionfile.write_text(a)
+versionfile.write_text(u'{}\n'.format(a))
 __version__ = versionfile.read_text().strip()
 
 if __name__ == '__main__':
